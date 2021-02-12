990b5da30efaa102bf0446aab381895ce91bc4ab
LEE337LEE <leegithinji@gmail.com>
2021-02-12 14:31:03 +0300

    completed profile component

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ProfileService  } from '../../services/profile.service'
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profile:any;
+  repos:any;
+  username:string;
+
+
+  constructor(private profileService: ProfileService) { 
+    this.profileService.getProfileInfo().subscribe(profile => {
+     console.log(profile); 
+     this.profile = profile;
+    });
+
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
+
+  }
+  findProfile(){
+    this.profileService.updateProfile(this.username);
+    this.profileService.getProfileInfo().subscribe(profile => {
+      console.log(profile); 
+      this.profile = profile;
+     });
+ 
+     this.profileService.getProfileRepos().subscribe(repos => {
+       console.log(repos);
+       this.repos = repos;
+     })
+  }
 
   ngOnInit(): void {
   }
 
 }
+  
+
